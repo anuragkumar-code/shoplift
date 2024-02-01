e843fcaf545455ab64eccfe22a30aef0b4e96cc6
anuragkumar-code <kumar.mairwa@gmail.com>
2024-02-01 22:54:52 +0530

    added sequelizer also for sql queries and models,not tested fully yet

--- server/db.js
+++ server/db.js
@@ -1,19 +1,20 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv').config();
+// const mysql = require('mysql2');
+// const dotenv = require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB
-});
+// const connection = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DB
+// });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL');
-});
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL:', err);
+//     return;
+//   }
+//   console.log('Connected to MySQL');
+// });
 
-module.exports = connection;
+// module.exports = connection;
+ 
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,37 +1,58 @@
-const db = require('../db');
+const { Sequelize, DataTypes } = require('sequelize');
+const config = require('../config/config.json');
 
-const createUserTable = () => {
-  const createTableQuery = `
-    CREATE TABLE IF NOT EXISTS users (
-      id INT PRIMARY KEY AUTO_INCREMENT,
-      firstname VARCHAR(255) NOT NULL,
-      lastname VARCHAR(255) NOT NULL,
-      mobile VARCHAR(255) NOT NULL,
-      password VARCHAR(255) NOT NULL,
-      email VARCHAR(255) NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-  `;
+const sequelize = new Sequelize(config.development);
 
-  return db.promise().query(createTableQuery);
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  firstname: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastname: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  mobile: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: 'users',
+  timestamps: true
+});
+
+
+const findUserByEmail = async (email) => {
+  return User.findOne({ where: { email } });
 };
 
-
-const findUserByEmail = (email) => {
-  const selectQuery = 'SELECT * FROM users WHERE email = ?';
-  
-  return db.promise().query(selectQuery, [email]);
+const findUserByMobile = async (mobile) => {
+  return User.findOne({ where: { mobile } });
 };
-
-const findUserByMobile = (mobile) => {
-  const selectQuery = 'SELECT * FROM users WHERE mobile = ?';
-  
-  return db.promise().query(selectQuery, [mobile]);
-};
-
 
 module.exports = {
-  createUserTable,
+  User,
   findUserByEmail,
   findUserByMobile,
 };
